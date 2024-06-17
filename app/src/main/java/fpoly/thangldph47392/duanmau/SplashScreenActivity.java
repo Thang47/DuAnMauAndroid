0646b1d59d64fb39b6384b197d8f51978274bb40
Thang47 <Thle36472003@gmail.com>
2024-06-17 21:27:18 +0700

    update time

--- app/src/main/java/fpoly/thangldph47392/duanmau/SplashScreenActivity.java
+++ app/src/main/java/fpoly/thangldph47392/duanmau/SplashScreenActivity.java
@@ -19,7 +19,7 @@ public class SplashScreenActivity  extends AppCompatActivity {
                 startActivity(i);
                 finish();
             }
-        }, 1500);
+        }, 2000);
 
 
 
